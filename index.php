de1d094cd9181b4b9ba8290e51f606baf39f2f0e
Tassos Koutlas <tassos.koutlas@gmail.com>
2015-10-20 20:43:22 +0100

    Added whitespace handling.

--- index.php
+++ index.php
@@ -4,6 +4,32 @@
  */
 
 /**
+ * Returns the first character in a string.
+ *
+ * @param string $string
+ *   A user specified string.
+ *
+ * @return string
+ *   The first character of user string.
+ */
+function get_first_char($string) {
+  return mb_substr($string, 0, 1);
+}
+
+/**
+ * Returns the last character in a string.
+ *
+ * @param string $string
+ *   A user specified string.
+ *
+ * @return string
+ *   The last character of user string.
+ */
+function get_last_char($string) {
+  return mb_substr($string, -1);
+}
+
+/**
  * Returns a string wrapped with new line characters length.
  *
  * @param string $string
@@ -35,48 +61,104 @@ function wrap($string, $length)
     $end = $length;
     $part = mb_substr($string, $start, $end);
     $partLength = mb_strlen($part);
-    $skip = false;
 
-    // If the last remaining characters skip rounding word
-    // splitting.
-    if ($partLength < $length) {
-      $skip = true;
+    // Setup first and last characters of string.
+    $firstChar = get_first_char($part);
+    $lastChar  = get_last_char($part);
+
+    // Setup total number of spaces and EOL characters.
+    $numberOfSpaces = mb_substr_count($part, $needle);
+    $numberOfEol    = mb_substr_count($part, PHP_EOL);
+
+    // Setup helpers to determine first and last lines.
+    $firstLine = false;
+    $lastLine  = false;
+
+    // Flag the first pass on the string.
+    if ($start == 1) {
+      $firstLine == true;
     }
 
-    // If splitting a word make sure the splitting starts in a new
-    // line.
+    // Flag the last pass on the string.
+    if ($partLength < $length) {
+      $lastLine = true;
+    }
 
-    // Check if sentence does not end in a space and there are spaces
-    // within it and if the skip flag is set to false.
-    if (!$skip && mb_substr($part, -1) !== $needle && mb_substr_count($part, $needle) > 0) {
+    // Handle word splitting.
 
-      // A character of ahead of current selection.
-      $ahead = mb_substr($string, $start + 1, 1);
+    // Check if it is not the last line, if the last character is not
+    // a space and if there are one or more spaces.
+    if (!$lastLine && $lastChar !== $needle && $numberOfSpaces > 0) {
+
+      // Get character ahead of current selection.
+      $ahead = mb_substr($string, $start + $end + 1, 1);
+      echo 'Hello';
 
       // Look at a character ahead and make sure that it is not a
       // space. Then go back find the position of the last space
       // and use it to calculate where to split the sentence so
       // that splitting a word starts in a new sentence. Then
       // use $kappa to update pointers accordingly.
-      if ($ahead != $needle) {
+      if ($ahead !== $needle) {
         $kappa = mb_strrpos($part, $needle);
-        $part = mb_substr($part, 0, $kappa);
+        $part = mb_substr($part, 0, $kappa) . '****';
         $end = $kappa + 1;
       }
 
+    }
+
+    // Handle whitespace
+
+    // If first character is a space. Calculate how many there are if
+    // more than two re-assign the string and start over.
+    if (!$lastLine && !$firstLine && $firstChar === $needle) {
+
+      $new_part = ltrim($part);
+      $delta = mb_strlen($part) - mb_strlen($new_part);
+
+      if ($delta > 1) {
+        $part = '';
+        $end = $delta;
+      } else {
+        $part = $new_part . '+++';
+      }
+
+    }
+
+    // If the last character is a space.
+    if (!$lastLine && mb_substr($part, -1) === $needle) {
+
+      $new_part = rtrim($part);
+      $delta = mb_strlen($part) - mb_strlen($new_part);
+
+      if ($delta > 1) {
+        $part = '';
+        $end = (-1) * $delta;
+      } else {
+        $part = $new_part;
+      }
+
+    }
+
+    // Handle PHP_EOL
+
+    // Get the first instace of PHP_EOL, calculate its index and get
+    // the part from the beggining to the index as the string. Update
+    // the pointers so next string just after the index.
+    if (!$lastLine && $numberOfEol) {
+      $kappa = mb_strpos($part, PHP_EOL);
+      $part = mb_substr($part, 0, $kappa);
+      $end = $kappa + 1;
     }
 
     // Update pointers.
     $start += $end;
 
     // Add string part to wrapped
-    $wrapped[] = $part;
+    if (!empty($part)) {
+      $wrapped[] = $part;
+    }
 
-  }
-
-  // Trim all whitespace from beggining and end of strings.
-  for ($i = 0; $i < count($wrapped); $i++) {
-    $wrapped[$i] = trim($wrapped[$i]);
   }
 
   // Return a string with new line characters.
@@ -113,12 +195,17 @@ function showcase($string) {
 /**
  * Sample lorem ipsum string.
  */ 
-$string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel mattis lectus, vitae aliquam nibh. Mauris nec malesuada est, vitae ornare tellus. Aenean vitae ex dui. Donec rutrum sapien ut convallis aliquam. Nam tincidunt pretium ante, nec porttitor sem. Praesent nibh velit, maximus vel tincidunt eu, facilisis id massa. Vivamus a enim vitae sem vulputate facilisis id ut nisi. Aliquam erat volutpat.";
+$string = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do:\nonce or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
 
 // Run showcase.
-//showcase($string);
+showcase($string);
 
-$wrapped = wrap('', 4);
+//$wrapped = wrap('anew   test     withh    lots of whitespace', 4);
 
-echo $wrapped;
+$wrap1 = wrap("    test", 10);
+$wrap2 = wrap('', 5);
+$wrap3 = wrap("test\ntest", 4);
 
+echo '1. ' .$wrap1 ."\n";
+echo '2. ' .$wrap2 ."\n";
+echo '3. ' .$wrap3 ."\n";
